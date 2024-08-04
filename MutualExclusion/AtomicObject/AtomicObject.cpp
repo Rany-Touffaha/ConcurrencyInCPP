ad2245ec77e42b78809d44954df62616d7540648
Rany-Touffaha <r_touffaha@hotmail.com>
2024-08-04 19:43:24 +0100

    Create program that uses an atomic object to avoid data race.

---
+++ MutualExclusion/AtomicObject/AtomicObject.cpp
@@ -0,0 +1,20 @@
+#include <thread>
+#include <atomic>
+
+using namespace std;
+
+atomic<unsigned int> garlic_count(0);
+
+void shopper() {
+    for (int i=0; i<10000000; i++) {
+        garlic_count++;
+    }
+}
+
+int main() {
+    thread joe(shopper);
+    thread jane(shopper);
+    joe.join();
+    jane.join();
+    printf("We should buy %u garlic.\n", garlic_count.load());
+}
